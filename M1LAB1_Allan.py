b4b6856dfe20401cbb51faf20ce3e54c0d1c42a4
LordAllan51 <74980052+LordAllan51@users.noreply.github.com>
2021-08-18 13:48:40 -0400

    Update M1LAB1_Allan.py

--- M1LAB1_Allan.py
+++ M1LAB1_Allan.py
@@ -9,17 +9,45 @@ def main():
     """Main Loop of program."""
     #print("Program goes here")
     print("This is the double a number program")
+    #TODP This should loop
+    keepgoing = 0
+    val = 0
+    while keepgoing == 0:
+        collectNums()
+        keepgoing = rep(val)
+    
+def collectNums():
     num = int(input("Enter a number: "))
     dbl = doubleAnumber(num)
-    print(num,"doubled is:",dbl)
-    
-    
+    print("A doubled ",num,"is:",dbl)
+
+
 def doubleAnumber(num):
     """input : one number
     output. the number * 2."""
     #return 0 # TODO make this actually work
     result = num * 2
     return result
+
+def rep(val):
+    while val == 0:
+        collectNums()
+        print("1. Enter another number")
+        print("2. Exit")
+        select = int(input("Enter your choice: "))
+        if select == 1:
+            val += 1
+            return (0)
+        elif select == 2:
+            val += 1
+            return (1)
+        else:
+            print("Invalid input.")
+        
+
+    
+    
+
 
 if __name__ == "__main__":
     main()
