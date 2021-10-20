04b8143759b7b7c10d2e625982f758ba11b04ec8
LordAllan51 <74980052+LordAllan51@users.noreply.github.com>
2021-10-20 13:52:03 -0400

    Add files via upload
    Norrisa template

---
+++ Github edit checker/github container norrisa 1.py
@@ -0,0 +1,55 @@
+# Container class
+# should be able to add, remove, list, and 
+# (optionally) transfer items
+#
+# we moved using a dictionary so we can easily
+# do name lookups
+from Item import Item
+
+class Container:
+    """ This class only handles collections of Items. """
+    
+    def __init__(self):
+        self.contents = {}
+        
+    def add(self, item):
+        self.contents[item.name] = item
+        
+    def remove(self, item):
+        if self.contains(item.name):
+            self.contents.remove(item)
+
+    def moveItemTo(self, item, destination):
+        
+        destination.add(item)
+        self.remove(item)
+        
+    def listContents(self):
+        for key in self.contents:
+            print(key)
+    """
+    This function is busted. I have an unholy mix
+    of item name and the item itself, and it's broken
+    """       
+    def contains(self, itemName):
+        """ quick way to check if item is present. """
+        # keys() gives us a list of names of items present
+        itemNameList = list(self.contents.keys())
+        if itemName in itemNameList:
+            return True
+        return False
+
+ 
+        
+def main():
+     """ test code"""
+     
+     
+ 
+    
+ 
+if __name__ == "__main__":
+    main()
+     
+     
+     
