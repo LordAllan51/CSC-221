72c79eb6708f0109a73e3548f0d5a5e4b9ad750d
LordAllan51 <74980052+LordAllan51@users.noreply.github.com>
2021-11-03 13:14:15 -0400

    Organization

--- M1T1_Allan.py
+++
@@ -1,9 +0,0 @@
-# CSC 221
-# M1T1
-# Lord Jenar Adolph C Allan
-# 8/16/2021
-
-print("Lord Jenar Allan")
-print("Python, Java, Swift")
-print("My interest is to learn how to code, how to create code, break codes,",
-      "\nand develop an app of my design and also help design that was required by the customer.")
